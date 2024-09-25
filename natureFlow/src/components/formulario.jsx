f84988d64397c8ac2f4b9aeb57c5467e7247f536
yesicaz <ymzemanate@soy.sena.edu.co>
2024-09-25 00:38:29 -0500

    configuraciones

---
+++ natureFlow/src/components/formulario.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const formulario = () => {
+  return (
+    <div>formulario</div>
+  )
+}
---
+++ natureFlow/src/routes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const routes = () => {
+  return (
+    <div>routes</div>
+  )
+}
